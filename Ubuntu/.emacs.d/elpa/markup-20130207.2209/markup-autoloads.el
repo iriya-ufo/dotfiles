4287619ea3c2c1fa54b7f14e9f0a19602782588a
Naoto Inoue <dorapocket22@gmail.com>
2013-10-19 03:26:00 +0900

    Installed markdown packages of Emacs.

---
+++ Ubuntu/.emacs.d/elpa/markup-20130207.2209/markup-autoloads.el
@@ -0,0 +1,18 @@
+;;; markup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("markup-pkg.el" "markup.el") (21089 31768
+;;;;;;  663830 555000))
+
+;;;***
+
+(provide 'markup-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; markup-autoloads.el ends here
---
+++ Ubuntu/.emacs.d/elpa/markup-20130207.2209/markup-pkg.el
@@ -0,0 +1,1 @@
+(define-package "markup" "20130207.2209" "Simple markup generation helpers. [github]" (quote nil))
