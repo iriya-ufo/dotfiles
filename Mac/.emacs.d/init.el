e2d2f2d2f031b76064fa3108898605f4d534d4e5
Naoto Inoue <dorapocket22@gmail.com>
2016-11-08 18:39:33 +0900

    web-mode custom-set-faces

--- Mac/.emacs.d/init.el
+++ Mac/.emacs.d/init.el
@@ -611,22 +611,22 @@ This is Scratch Buffer.
 (add-hook 'web-mode-hook 'web-mode-hook)
 ;; 色の設定
 (custom-set-faces
- '(web-mode-doctype-face
-   ((t (:foreground "#82AE46"))))               ; doctype
- '(web-mode-html-tag-face
-   ((t (:foreground "#E6B422" :weight bold))))  ; 要素名
- '(web-mode-html-attr-name-face
-   ((t (:foreground "#C97586"))))               ; 属性名など
- '(web-mode-html-attr-value-face
-   ((t (:foreground "#82AE46"))))               ; 属性値
- '(web-mode-comment-face
-   ((t (:foreground "#D9333F"))))               ; コメント
- '(web-mode-server-comment-face
-   ((t (:foreground "#D9333F"))))               ; コメント
- '(web-mode-css-rule-face
-   ((t (:foreground "#A0D8EF"))))               ; cssのタグ
- '(web-mode-css-pseudo-class-face
-   ((t (:foreground "#FF7F00"))))               ; css 疑似クラス
- '(web-mode-css-at-rule-face
-   ((t (:foreground "#FF7F00"))))               ; cssのタグ
-)
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(web-mode-comment-face ((t (:foreground "#D9333F"))))
+ '(web-mode-css-at-rule-face ((t (:foreground "#FF7F00"))))
+ '(web-mode-css-pseudo-class-face ((t (:foreground "#FF7F00"))))
+ '(web-mode-css-rule-face ((t (:foreground "#A0D8EF"))))
+ '(web-mode-doctype-face ((t (:foreground "#82AE46"))))
+ '(web-mode-html-attr-name-face ((t (:foreground "#C97586"))))
+ '(web-mode-html-attr-value-face ((t (:foreground "#82AE46"))))
+ '(web-mode-html-tag-face ((t (:foreground "#E6B422" :weight bold))))
+ '(web-mode-server-comment-face ((t (:foreground "#D9333F")))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (package-build shut-up epl git commander f dash s))))
