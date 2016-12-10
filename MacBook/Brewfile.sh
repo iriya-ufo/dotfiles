3adf8f4bdd8a87a698d4ae86833758cb0e8b476f
iriya <iriya@iriyanoMacBook.local>
2016-12-11 02:42:23 +0900

    update brewfile

---
+++ MacBook/Brewfile.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+brew tap caskroom/cask
+brew cask install iterm2
+brew cask install google-chrome
+brew cask install dropbox
+brew cask install evernote
+brew cask install google-japanese-ime
+brew cask install gyazo
+brew cask install slack
+brew cask install thunderbird
+brew cask install atom
+brew cask install quicksilver
+brew cask install google-photos-backup
+brew cask install insync
+brew cask install docker
+brew cask install cmd-eikana
+brew cask install shiftit
+
+brew install ansible
+brew install asciinema
+brew install colordiff
+brew install coreutils
+brew install gauche
+brew install git
+brew install git-secrets
+brew install graphviz
+brew install imagemagick
+brew install keychain
+brew install lv
+brew install nkf
+brew install nmap
+brew install postgresql
+brew install pstree
+brew install rbenv
+brew install rbenv-gemset
+brew install reattach-to-user-namespace
+brew install rlwrap
+brew install the_silver_searcher
+brew install tig
+brew install tmux
+brew install wget
+brew install mysql
+brew install zsh
+brew install zsh-completions
+brew install doxygen
