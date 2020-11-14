e4cd0bdae5b611f601ff9b89d196c2e0f84efe5f
Naoto Inoue <dorapocket22@gmail.com>
2020-11-14 12:28:30 +0900

    Remove eikana Install karabiner

--- Mac/Brewfile.sh
+++ Mac/Brewfile.sh
@@ -11,9 +11,9 @@ brew cask install thunderbird
 brew cask install alfred
 brew cask install google-photos-backup-and-sync
 brew cask install insync
+brew cask install karabiner-elements
 brew cask install discord
 brew cask install docker
-brew cask install cmd-eikana
 brew cask install shiftit
 brew cask install ngrok
 brew cask install postman
