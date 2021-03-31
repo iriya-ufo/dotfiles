42abd444ba6a53b80febd2942d77b984921a5190
Naoto Inoue <dorapocket22@gmail.com>
2021-03-31 14:56:22 +0900

    Use straight.el to maintain packages

---
+++ Mac/.emacs.d/init.el
@@ -0,0 +1,31 @@
+;; -----------------------------------------------------------------------------------------------
+;; brief   : straight.el: next-generation, purely functional package manager for the Emacs hacker.
+;; note    : https://github.com/raxod502/straight.el
+;; -----------------------------------------------------------------------------------------------
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Users of Emacs versions >= 27 will want to add:
+(setq package-enable-at-startup nil)
+
+;; Integration with use-package
+(straight-use-package 'use-package)
+(setq straight-use-package-by-default t)
+
+;; -----------------------------------------------------------------------------------------------
+;; brief   : init-loader configuration
+;; note    : -
+;; -----------------------------------------------------------------------------------------------
+(use-package init-loader)
+(setq init-loader-show-log-after-init nil)
+(init-loader-load "~/.emacs.d/inits")
---
+++ Mac/.emacs.d/straight/versions/default.el
@@ -0,0 +1,8 @@
+(("el-get" . "84dd1837f9ac80a329ab0c2de6859777f445f8ff")
+ ("emacsmirror-mirror" . "9f00df1a00d97bbcdb9a820e0941f9196a71ec3d")
+ ("gnu-elpa-mirror" . "c9bc5582228c82db01617802b8729ca86a76bf79")
+ ("init-loader" . "10b8d7b12acbd1036e68dfb59f460714baedaa33")
+ ("melpa" . "380125490f47bd150218280c2e16c01be9054a60")
+ ("straight.el" . "e1390a933b6f5a15079d6dec91eac97a17aad10c")
+ ("use-package" . "a7422fb8ab1baee19adb2717b5b47b9c3812a84c"))
+:beta
