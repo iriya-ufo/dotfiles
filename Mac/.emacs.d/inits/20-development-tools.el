9235737f38b581b7c1a01d51ebebfa8976e7ed52
Naoto Inoue <dorapocket22@gmail.com>
2021-04-01 18:06:52 +0900

    Added dev tools to emacs

---
+++ Mac/.emacs.d/inits/20-development-tools.el
@@ -0,0 +1,11 @@
+;; -----------------------------------------------------------------------------------------------
+;; brief   : flycheck configuration
+;; note    : -
+;; -----------------------------------------------------------------------------------------------
+(use-package flycheck)
+(use-package flycheck-color-mode-line)
+(use-package flycheck-pos-tip)
+
+(require 'flycheck)
+(global-flycheck-mode t)
+(require 'flycheck-color-mode-line)
