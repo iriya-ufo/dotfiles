05c5307262da02f35c618621edfc285f13b8fee5
Naoto Inoue <dorapocket22@gmail.com>
2021-04-01 11:00:47 +0900

    Change auto complete setting on emacs

--- Mac/.emacs.d/inits/21-auto-complete.el
+++ Mac/.emacs.d/inits/21-auto-complete.el
@@ -5,5 +5,12 @@
 (use-package auto-complete)
 (require 'auto-complete)
 (require 'auto-complete-config)
+
+(setq ac-dwim t)        ; useful configuration
+(setq ac-auto-start 4)  ; start trigger: input 4 characters
+(setq ac-comphist-file "~/.emacs.d/cache/ac-comphist.dat")
+
+(ac-set-trigger-key "TAB")
+(define-key ac-completing-map (kbd "C-n") 'ac-next)
+(define-key ac-completing-map (kbd "C-p") 'ac-previous)
 (global-auto-complete-mode t)
-(setq ac-auto-start 4)  ; 4文字以上の際に補完を開始する
