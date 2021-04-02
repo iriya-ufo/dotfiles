9672741a46d38554459d68d7cd16c241aa888868
Naoto Inoue <dorapocket22@gmail.com>
2021-04-02 17:08:31 +0900

    Added ricty font

--- Mac/.emacs.d/inits/02-appearance.el
+++ Mac/.emacs.d/inits/02-appearance.el
@@ -20,10 +20,29 @@
 (setq inhibit-startup-message t)
 (setq initial-scratch-message "")
 
-;; 半透明化
+;; translucent
 (if window-system
     (progn
       (set-frame-parameter nil 'alpha 86)))
+
+;; ------------------------------------------------------------------
+;; brief   : フォント設定
+;; note    : 事前にフォントのインストールが必要
+;;         : $ brew tap homebrew/cask-fonts
+;;         : $ brew install --cask font-ricty-diminished
+;; ------------------------------------------------------------------
+(set-face-attribute 'default nil
+                    :family "Ricty Diminished Discord"
+                    :height 140)
+(set-fontset-font (frame-parameter nil 'font)
+                  'japanese-jisx0208
+                  (cons "Ricty Diminished Discord" "iso10646-1"))
+(set-fontset-font (frame-parameter nil 'font)
+                  'japanese-jisx0212
+                  (cons "Ricty Diminished Discord" "iso10646-1"))
+(set-fontset-font (frame-parameter nil 'font)
+                  'katakana-jisx0201
+                  (cons "Ricty Diminished Discord" "iso10646-1"))
 
 ;; ------------------------------------------------------------------
 ;; brief   : popwin
