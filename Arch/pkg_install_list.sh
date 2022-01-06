7fe742a23265f0b505fd4008c8c0756b58ee0290
Naoto Inoue <dorapocket22@gmail.com>
2022-01-07 02:34:49 +0900

    Edit Arch install list

---
+++ Arch/pkg_install_list.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+paru colordiff \
+cmigemo \
+emacs \
+fzf \
+gauche \
+ghq \
+git \
+git-secrets \
+go \
+jq \
+lazygit \
+lsof \
+lv \
+nkf \
+nmap \
+nodenv \
+pyenv \
+rbenv \
+rbenv-gemset \
+rlwrap \
+the_silver_searcher \
+tig \
+tree \
+wget \
+zsh \
+zsh-completions \
+zsh-syntax-highlighting
