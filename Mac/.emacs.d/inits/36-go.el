984328eb3b4e7790efa1283afd6225b328c84949
Naoto Inoue <dorapocket22@gmail.com>
2022-02-18 15:36:57 +0900

    Added goimports

--- Mac/.emacs.d/inits/36-go.el
+++ Mac/.emacs.d/inits/36-go.el
@@ -4,3 +4,11 @@
 
 (use-package go-mode)
 (add-hook 'go-mode-hook #'lsp)
+
+;; ------------------------------------------------------------------
+;; brief   : goimports
+;; note    : https://pkg.go.dev/golang.org/x/tools/cmd/goimports
+;;         : $ go install golang.org/x/tools/cmd/goimports@latest
+;; ------------------------------------------------------------------
+(setq gofmt-command "goimports")
+(add-hook 'before-save-hook 'gofmt-before-save)
