77ccdd82d876dd80fa9fcece3f33647177ef1452
Naoto Inoue <dorapocket22@gmail.com>
2022-02-22 23:07:30 +0900

    Go compile settings for Emacs

--- Mac/.emacs.d/inits/36-go.el
+++ Mac/.emacs.d/inits/36-go.el
@@ -6,9 +6,17 @@
 (add-hook 'go-mode-hook #'lsp)
 
 ;; ------------------------------------------------------------------
-;; brief   : goimports
-;; note    : https://pkg.go.dev/golang.org/x/tools/cmd/goimports
+;; brief   : Adding useful tips by go-mode-hook
+;; note    : You should install goimports for formatting
 ;;         : $ go install golang.org/x/tools/cmd/goimports@latest
 ;; ------------------------------------------------------------------
-(setq gofmt-command "goimports")
-(add-hook 'before-save-hook 'gofmt-before-save)
+(defun go-mode-omnibus ()
+  ;; Go code formatting by goimports
+  (setq gofmt-command "goimports")
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  ;; Customize compile command to run go build
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
+  )
+(add-hook 'go-mode-hook 'go-mode-omnibus)
