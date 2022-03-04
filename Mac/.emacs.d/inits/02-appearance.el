b0afee2c903e6903589b9f08c9ad2204533c8505
Naoto Inoue <dorapocket22@gmail.com>
2022-03-04 21:09:00 +0900

    Added neotree

--- Mac/.emacs.d/inits/02-appearance.el
+++ Mac/.emacs.d/inits/02-appearance.el
@@ -49,6 +49,51 @@
                   (cons "Ricty Diminished Discord" "iso10646-1"))
 
 ;; ------------------------------------------------------------------
+;; brief   : Neotree
+;; note    : https://github.com/jaypei/emacs-neotree
+;; ------------------------------------------------------------------
+(use-package neotree
+  :init
+  (setq-default neo-keymap-style 'concise)
+  (setq-default neo-show-hidden-files t)
+  :config
+  (setq neo-smart-open t)
+  (setq neo-create-file-auto-open t)
+  (setq neo-theme 'arrow)
+  (bind-key "C-c C-o" 'neotree-toggle)
+  (bind-key "RET" 'neotree-enter-hide neotree-mode-map)
+  (bind-key "a" 'neotree-hidden-file-toggle neotree-mode-map)
+  (bind-key "<left>" 'neotree-select-up-node neotree-mode-map)
+  (bind-key "<right>" 'neotree-change-root neotree-mode-map))
+
+;; Change neotree's font size
+;; Tips from https://github.com/jaypei/emacs-neotree/issues/218
+(defun neotree-text-scale ()
+  "Text scale for neotree."
+  (interactive)
+  (text-scale-adjust 0)
+  (text-scale-decrease 0.5)
+  (message nil))
+(add-hook 'neo-after-create-hook
+          (lambda (_)
+            (call-interactively 'neotree-text-scale)))
+
+;; Hide neotree window after open file
+;; Tips from https://github.com/jaypei/emacs-neotree/issues/77
+(defun neo-open-file-hide (full-path &optional arg)
+  "Open file and hiding neotree.
+The description of FULL-PATH & ARG is in `neotree-enter'."
+  (neo-global--select-mru-window arg)
+  (find-file full-path)
+  (neotree-hide))
+
+(defun neotree-enter-hide (&optional arg)
+  "Neo-open-file-hide if file, Neo-open-dir if dir.
+The description of ARG is in `neo-buffer--execute'."
+  (interactive "P")
+  (neo-buffer--execute arg 'neo-open-file-hide 'neo-open-dir))
+
+;; ------------------------------------------------------------------
 ;; brief   : popwin
 ;; note    : https://github.com/emacsorphanage/popwin
 ;; ------------------------------------------------------------------
