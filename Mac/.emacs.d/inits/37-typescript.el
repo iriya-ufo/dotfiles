5e2e0435a86e71b44fda5de5f9d8e3df6d6880b6
Naoto Inoue <dorapocket22@gmail.com>
2023-02-15 11:03:55 +0900

    Modified ts mode

--- Mac/.emacs.d/inits/37-typescript.el
+++ Mac/.emacs.d/inits/37-typescript.el
@@ -9,7 +9,16 @@
 ;;         : Install this language server with M-x lsp-install-server RET ts-ls RET.
 ;; ------------------------------------------------------------------
 (use-package typescript-mode)
+(use-package tide)
 (require 'typescript-mode)
 (add-to-list 'auto-mode-alist '("\.ts$" . typescript-mode))
-(add-hook 'typescript-mode-hook '(lambda () (setq typescript-indent-level 2)))
+(add-hook 'typescript-mode-hook
+          '(lambda ()
+             (interactive)
+             (tide-setup)
+             (flycheck-mode +1)
+             (tide-hl-identifier-mode +1)
+             (company-mode +1)
+             (eldoc-mode +1)
+             ))
 (add-hook 'typescript-mode-hook #'lsp)
