0bafeac032db3f987d0f8d1a3fdc504f20419dd2
Naoto Inoue <dorapocket22@gmail.com>
2023-02-19 22:22:55 +0900

    Delete white space

--- M2-Mac/.emacs.d/inits/37-rust.el
+++ M2-Mac/.emacs.d/inits/37-rust.el
@@ -5,7 +5,7 @@
 ;; ------------------------------------------------------------------
 ;; brief   : rustic-mode is an extension of rust-mode that provides better support for the Rust language
 ;; note    : https://github.com/brotzeit/rustic
-;;         : $ brew install rust-analyzer 
+;;         : $ brew install rust-analyzer
 ;; usage   : https://iriya-ufo.net/blog/2023/02/18/rust-setup-on-emacs/
 ;; ------------------------------------------------------------------
 (use-package rustic)
