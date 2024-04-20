b06a865c96ea1e441f9f108d4716986347801066
Naoto Inoue <dorapocket22@gmail.com>
2024-04-21 00:49:52 +0900

    Disable Emacs vc git tool

--- M2-Mac/.emacs.d/inits/22-magit.el
+++ M2-Mac/.emacs.d/inits/22-magit.el
@@ -2,5 +2,10 @@
 ;;; Commentary:
 ;;; Code:
 
+;; magit を利用するので Emacs 標準のバージョン管理ツールを使用しない
+(setq vc-handled-backends nil)
+(eval-after-load "vc"
+  '(remove-hook 'find-file-hooks 'vc-find-file-hook))
+
 (use-package magit)
 (global-set-key (kbd "C-x g") 'magit-status)
