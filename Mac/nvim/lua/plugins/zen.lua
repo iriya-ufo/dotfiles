cfbd880e330c73b2d30323a2b1bf9fac7bed3ae1
Naoto Inoue <dorapocket22@gmail.com>
2024-10-10 00:37:12 +0900

    Added ZenMode to LazyVim

---
+++ Mac/nvim/lua/plugins/zen.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    keys = {
+      { "<leader>uz", "<cmd>ZenMode<cr>", desc = "Toggle Zen Mode" },
+    },
+    opts = {},
+  },
+}
